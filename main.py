59b5e0d58eae3f0e9b1133a674245ddd1b78773d
Dominik Häußer <haeusser-dominik@web.de>
2023-09-06 09:06:43 +0200

    implement GameBoard

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import pygame as pg
 
 
@@ -17,73 +19,46 @@ GAME_BOARD_2 = [
     [1, 1, 0]
 ]
 
-# class Tile:
-#     def __init__(self, x, y, square_lengths, color=(255,255,255), border_thickness=4):
-#         self.x = x
-#         self.y = y
-#         self.width = square_lengths
-#         self.height = square_lengths
-#         self.color = color
-#         self.border_thickness = border_thickness
 
-#     def draw(self, window):
-#         pg.draw.rect(window, (0, 0, 0), (self.x, self.y, self.width, self.height))
-#         pg.draw.rect(
-#             window,
-#             self.color, 
-#             (self.x+self.border_thickness, 
-#              self.y+self.border_thickness, 
-#              self.width-self.border_thickness*2, 
-#              self.height-self.border_thickness*2)
-#         )
-
-class Tile(Sprite):
+class Tile(pg.sprite.Sprite):
     def __init__(self, size, pos, color=(255, 255, 255)):
         super().__init__()
-        self.surface = pg.Surface(size)
-        self.surface.fill((0, 0, 0))
-        self.rect = self.surface.get_rect(center=pos)
-        self.inner_surface = Surface([size/1.2, size/1.2])
-        self.inner_surface.fill(color)
-        self.inner_rect = self.inner_surface.get_rect(center=[size/2, size/2])
-        self.surface.blit(self.inner_surface, self.inner_surface)
-# test the tile class by adding it to a group and then print
-# it to the screen!
+        self.image = pg.Surface([size, size])
+        self.image.fill((0, 0, 0))
+        self.rect = self.image.get_rect(topleft=pos)
+        self.inner_image = pg.Surface([size/1.1, size/1.1])
+        self.inner_image.fill(color)
+        self.inner_rect = self.inner_image.get_rect(center=[size/2, size/2])
+        self.image.blit(self.inner_image, self.inner_rect)
 
-# rewrite like in hitTheBlock! with the new tile class
+
+class GameBoard:
+    def __init__(self, x, y, game_board_list):
+        self.x = x
+        self.y = y
+        self.game_board_list = game_board_list
+        self.tiles = pg.sprite.Group()
+        self._init_board_cols()
+
+    def _init_board_cols(self):
+        for row_num in range(len(self.game_board_list)):
+            for col_num, col in enumerate(self.game_board_list[row_num]):
+                if col:
+                    tile = Tile(TILE_WIDTH, (self.x + col_num*TILE_WIDTH, self.y + row_num*TILE_WIDTH))
+                    self.tiles.add(tile)
+
+    def draw(self, window):
+        self.tiles.draw(window)
+
+# write like in hitTheBlock! with the new tile class
 # es müssen tiles hinzugefügt werden können
 # diese müssen außerdem auf ein neues surface geschrieben
 # werden damit dieses als ganzes rotiert werden kann!
 # rotate surface https://stackoverflow.com/questions/4183208/how-do-i-rotate-an-image-around-its-center-using-pygame
 # flip image:  https://www.geeksforgeeks.org/pygame-flip-the-image/
 
-class GameBoard:
-    def __init__(self, x, y, board_rows):
-        self.x = x
-        self.y = y
-        self.board_rows = board_rows
-        self.tiles = []
-        self._init_board_cols()
-
-    def _init_board_cols(self):
-        for row_num in range(len(self.board_rows)):
-            tile_row = []
-            for col_num, col in enumerate(self.board_rows[row_num]):
-                if col:
-                    tile = Tile(self.x + col_num*TILE_WIDTH, self.y + row_num*TILE_WIDTH, TILE_WIDTH)
-                    tile_row.append(tile)
-            self.tiles.append(tile_row)
-
-    def draw(self, window):
-        for row in self.tiles:
-            for tile in row:
-                tile.draw(window)
 
 
-class PlayingPiece:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
 
 
 def main():
@@ -92,16 +67,35 @@ def main():
     window = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pg.display.set_caption("Ubongo Solver")
 
-    # game_board = GameBoard(100, 100, GAME_BOARD_1)
+    game_board = GameBoard(0, 0, GAME_BOARD_1)
+    # tile = Tile(TILE_WIDTH, (10, 10))
+    # tiles_group = pg.sprite.Group()
+    # tiles_group.add(tile)
+    # piece = PlayingPiece((150, 150), [0, 0, 0])
+    # playing_pieces_group = pg.sprite.Group()
+    # playing_pieces_group.add(piece)
     
+    clock = pg.time.Clock()
+    fps = 1
+
     run = True
     while run:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
         window.fill((255, 100, 100))
+
         game_board.draw(window)
+        
+        # playing_pieces_group.update()
+        # piece.rotate()
+        # playing_pieces_group.draw(window)
+        
+        # tiles_group.draw(window)
+        
         pg.display.flip()
+
+        clock.tick(fps)
 
 
 if __name__ == '__main__':
